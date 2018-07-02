405832ff30e14d78cb13819e3749c5c84c791da6
Charles Rethman <rethman@me.com>
2018-07-02 07:05:31 +0200

    Added in IfElse Conditionals

---
+++ 12_if_else-if_else/01_eval-true/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if true {
+		fmt.Println("this ran")
+	}
+
+	if false {
+		fmt.Println("This did not run")
+	} else {
+		fmt.Println("but this did")
+	}
+}
