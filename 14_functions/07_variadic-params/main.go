69c8e00276033fc05d0c1816ca2458da2e8c527d
Charles Rethman <rethman@me.com>
2018-07-18 11:10:11 +0200

    Added in variadics and func expressions

---
+++ 14_functions/07_variadic-params/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := average(43, 56, 87, 12, 45, 57)
+	fmt.Println(n)
+}
+
+func average(sf ...float64) float64 {
+	total := 0.0
+	fmt.Println(sf)
+	fmt.Printf("%T\n", sf)
+	for _, v := range sf {
+		total += v
+	}
+	// fmt.Println(total)
+	return total / float64(len(sf))
+}
